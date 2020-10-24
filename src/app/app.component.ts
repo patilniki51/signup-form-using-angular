c539d45f42a49f705b4982abc6c51dd7058a6abe
Nikita <patilniki51@gmail.com>
2020-10-24 12:03:54 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup, Validators} from '@angular/forms';
-import { PasswordChecker } from './custom-validators/PasswordChecker';
+import { Component, OnInit } from "@angular/core";
+
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { PasswordChecker } from "./custom-validators/PasswordChecker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  title = 'signup-reactive-form';
+  title = "signup-reactive";
+
   registerForm: FormGroup;
-  submitted:boolean = false;
-  
-  constructor(private formBuilder: FormBuilder){
+  submitted = false;
 
-  }
+  constructor(private formbuilder: FormBuilder) {}
 
-  ngOnInit(){
-    this.registerForm = this.formBuilder.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required],
-      acceptTandC: [false, Validators.requiredTrue]
-    },{
-      validators: PasswordChecker('password','confirmPassword')
+  ngOnInit() {
+    this.registerForm = this.formbuilder.group({
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ["", Validators.required],
+      acceptTandC: [false, Validators.requiredTrue],
+    }, {
+      validators: PasswordChecker("password", "confirmPassword"),
     });
   }
 
-  get form(){
+  get h() {
     return this.registerForm.controls;
   }
-  onSubmit(){
+
+  onSubmit() {
     this.submitted = true;
-    if(this.registerForm.invalid){
-      return " Please fill in the required fields";
+    if (this.registerForm.invalid) {
+      return;
     }
 
     console.table(this.registerForm.value);
     console.table(this.registerForm);
 
-    alert(
-      "Signed in successfully \n" + JSON.stringify(this.registerForm)
-    );
+    alert("Success Signup\n" + JSON.stringify(this.registerForm.value));
   }
 
-  onReset(){
+  onReset() {
     this.submitted = false;
     this.registerForm.reset();
   }
-
 }
--- src/app/custom-validators/PasswordChecker.ts
+++ src/app/custom-validators/PasswordChecker.ts
@@ -1,13 +1,17 @@
-import { FormGroup } from '@angular/forms';
+import { FormGroup } from "@angular/forms";
 
-export function PasswordChecker(controlName: string, confControlName: string){
-    return (formGroup: FormGroup) => {
-        const password = formGroup.controls[controlName];
-        const confPassword = formGroup.controls[confControlName];
-        if(password !== confPassword){
-            confPassword.setErrors({ mustMatch: true});
-        }else{
-            confPassword.setErrors(null);
-        }
-    };
+export function PasswordChecker(
+  controlName: string,
+  CompareControlName: string,
+) {
+  return (formGroup: FormGroup) => {
+    const password = formGroup.controls[controlName];
+    const confPassword = formGroup.controls[CompareControlName];
+
+    if (password.value !== confPassword.value) {
+      confPassword.setErrors({ mustmatch: true });
+    } else {
+      confPassword.setErrors(null);
+    }
+  };
 }
